44cba18c09d4475476d61530bab36fadb093eaab
weiyongyuan <weiyongyuan@aliyun.com>
2016-07-08 22:11:41 +0800

    update3

--- src/com/atguigu/tag/TestGit.java
+++ src/com/atguigu/tag/TestGit.java
@@ -3,6 +3,6 @@ package com.atguigu.tag;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("hello2");
+		System.out.println("hello3");
 	}
 }
